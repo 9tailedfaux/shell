5cbd9628cbe7bf03519264c1903ac661838a8f78
CaseyRobertsonWork <wdrrcaseyr@iastate.edu>
2022-02-27 18:02:35 -0600

    off to a horrible start

--- shell.c
+++ shell.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 
-void prompt();
+int prompt();
 
 int main(int argc, char** argv) {
-	prompt(argv[1]);
+	char* defprompt = "308sh> ";
+	while (1) {
+		char in[100];
+		prompt(defprompt, in, strlen(in));
+	}
 }
 
-void prompt(char* prompt){
-	prints(prompt);
+//returns 0 if totes okay, 1 if the user input nothing, 2 if the user input was too long
+int prompt(char* prompt, char* buffer, size_t size){
+	int ch;
+	int toolong;
+	if (prompt != NULL) {
+		printf ("%s", prompt);
+		fflush (stdout);
+	}
+	if (fgets(buffer, size, stdin) == NULL) {
+		return 1;
+	}
+
+	if (buffer[strlen(buffer) - 1] != '\n') {
+		toolong = 0;
+		while(((ch = getchar()) != '\n') && (ch != EOF)) {
+			toolong = 1;
+		}
+		return (toolong) ? 2 : 0;
+	}
+	
+	buffer[strlen(buffer) - 1] = '\0';
+	return 0;
 }
